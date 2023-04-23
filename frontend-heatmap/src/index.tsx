d5992f5fde2a1d3ecc0f1dda4575a3aafce4313f
Chief <>
2023-04-23 09:57:19 +0300

    frontend-heatmap / added header

--- frontend-heatmap/src/index.tsx
+++ frontend-heatmap/src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Header } from "./features";
+import { BrowserRouter } from "react-router-dom";
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -8,9 +10,10 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Header/>
+    <App/>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
