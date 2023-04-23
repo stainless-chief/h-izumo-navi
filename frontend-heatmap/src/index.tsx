ad2bf25b0b9866d0e621865a2eb895a9748ec2a5
Chief <>
2023-04-23 10:39:40 +0300

    frontend-heatmap / cleanup

--- frontend-heatmap/src/index.tsx
+++ frontend-heatmap/src/index.tsx
@@ -4,8 +4,6 @@ import { Header } from "./features";
 import { BrowserRouter } from "react-router-dom";
 import './index.scss';
 import { AppRouter } from "./AppRouter";
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -16,8 +14,3 @@ root.render(
     <AppRouter />
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
