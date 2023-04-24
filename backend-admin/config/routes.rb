49e7e59c9d04942aaf5c798e042b45976c5e0e67
wishka <roucke@mail.ru>
2023-04-24 14:14:58 +0300

    Add user roles and views

--- backend-admin/config/routes.rb
+++ backend-admin/config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
