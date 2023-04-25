03e9f0d06312377f75548810e58c87e4bdf3d1fe
Chief <>
2023-04-25 19:08:05 +0300

    fronted-heatmap / Added custom env handling

---
+++ frontend-heatmap/public/env-config.js
@@ -0,0 +1,4 @@
+window._env_ = {
+  REACT_APP_MAPBOX_API_KEY: "pk.eyJ1IjoiY2hpZWYtc3MiLCJhIjoiY2xndGVkNnp3MTV0bjNkbWtzc25sdDlodiJ9.IigjwyBzw-Z_FdBd3eW1tg",
+  REACT_APP_API_ANALYTICS: "http://localhost:5223/analytics/v1.0/"
+};
---
+++ frontend-heatmap/src/window.d.ts
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-unused-vars
+interface Window {
+  _env_ : any
+}
