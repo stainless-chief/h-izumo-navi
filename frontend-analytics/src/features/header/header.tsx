2df8e1a4b52ec6f9b78a5f4164483d97a15dd43f
Chief <>
2023-05-28 17:37:45 +0900

    frontend-analytics / changed header behavior

--- frontend-analytics/src/features/header/header.tsx
+++ frontend-analytics/src/features/header/header.tsx
@@ -2,13 +2,32 @@ import "./header.scss";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import "../../locales/i18n";
+import { useLocation } from 'react-router-dom';
 
 function Header() {
   const { t } = useTranslation();
+  const { pathname } = useLocation();
+  var text = t("Header.Analytics");
+
+  switch(pathname)
+  {
+    case "/heatmap": {
+      text = t("Header.Heatmap");
+      break;
+    }
+    case "/statistics": {
+      text = t("Header.Statistics");
+      break;
+    }
+    case "/predict": {
+      text = t("Header.MachineLearning");
+      break;
+    }
+  }
 
   return (
   <header>
-    <Link className="full-size" to="/"><b>{t("Header.Full")}</b></Link>
+    <Link className="full-size" to="/"><b>{text}</b></Link>
   </header>
   );
 }
