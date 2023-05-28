83ca45c01562d7f1afc0a9d3b1bdd7a4c62ec7ce
wishka <roucke@mail.ru>
2023-05-28 10:31:55 +0900

    remove trash

--- backend-admin/app/controllers/admin_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdminController < ApplicationController
-  def dashboard
-    if current_user&.admin?
-      @messages = Message.messages_this_month
-    else
-      redirect_to root_path
-    end
-  end
-end
